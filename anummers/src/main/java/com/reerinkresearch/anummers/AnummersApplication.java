5aeea4cba3a7d29a6dab62d7317269c27e501d97
Frank Reerink <freerink@tmns.com>
2021-01-26 21:31:26 +0100

    Add anummers PostMapping

--- anummers/src/main/java/com/reerinkresearch/anummers/AnummersApplication.java
+++ anummers/src/main/java/com/reerinkresearch/anummers/AnummersApplication.java
@@ -3,6 +3,8 @@ package com.reerinkresearch.anummers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,15 @@ public class AnummersApplication {
 			long skipTo = a.getSkipTo();
 			a = new Anummer(counter.incrementAndGet(), skipTo);
 			iter++;
+		}
+		return a;
+	}
+	
+	@PostMapping("/anummers")
+	Anummer storeAnummer(@RequestBody Anummer a) {
+		// Check if we received a valid A nummer
+		if( ! a.isValid() ) {
+			throw new InvalidAnummerException(a);
 		}
 		return a;
 	}
