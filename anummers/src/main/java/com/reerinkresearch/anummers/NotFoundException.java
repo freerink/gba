cc2b03c823506f39b548b7efce5361e15f788b57
Frank Reerink <freerink@tmns.com>
2021-02-12 16:50:51 +0100

    More generic exception

--- anummers/src/main/java/com/reerinkresearch/anummers/NotFoundException.java
+++ anummers/src/main/java/com/reerinkresearch/anummers/NotFoundException.java
@@ -2,7 +2,7 @@ package com.reerinkresearch.anummers;
 
 public class NotFoundException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 8397614277344895892L;
 
 	public NotFoundException(String message) {
 		super("Not found: " + message);
