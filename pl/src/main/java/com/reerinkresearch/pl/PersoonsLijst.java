c4f35e54833a1c91624d84a51c80a747e960f199
Frank Reerink <freerink@tmns.com>
2021-02-24 22:00:39 +0100

    pl validatie

--- pl/src/main/java/com/reerinkresearch/pl/PersoonsLijst.java
+++ pl/src/main/java/com/reerinkresearch/pl/PersoonsLijst.java
@@ -6,52 +6,70 @@ import java.util.List;
 public class PersoonsLijst {
 
 	private List<Persoon> persoon;
-	
+
 	// Ouder1 -> index = 0
 	// Ouder2 -> index = 1
 	private List<List<Ouder>> ouder;
 
 	private Inschrijving inschrijving;
-	
+
 	private List<Verblijfplaats> verblijfplaats;
-	
+
 	/*
 	 * Constructor
 	 */
 	public PersoonsLijst() {
 	}
-	
+
 	public PersoonsLijst(long anummer, String geslachtsnaam, int gemeenteCode) {
 		this.persoon = Persoon.createMinimumPersoonStapel(anummer, geslachtsnaam);
 		this.verblijfplaats = Verblijfplaats.createMinimumVerblijfplaatsStapel(gemeenteCode);
 	}
-	
+
+	public boolean isValid() {
+		// At least one Person record
+		if (this.getPersoon() == null || this.getPersoon().size() == 0) {
+			return false;
+		}
+		// We need a valid A number
+		Anummer anummer = new Anummer(this.getPersoon().get(0).getAnummer());
+		if (!anummer.isValid()) {
+			return false;
+		}
+		if (this.getPersoon().get(0).getNaam() == null
+				|| this.getPersoon().get(0).getNaam().getGeslachtsnaam() == null
+				|| this.getPersoon().get(0).getNaam().getGeslachtsnaam().length() < 1) {
+			return false;
+		}
+		return true;
+	}
+
 	/*
 	 * Getters / Setters
 	 */
 	public void setAnummer(long anummer) {
-		if( this.persoon == null) {
+		if (this.persoon == null) {
 			this.persoon = new ArrayList<Persoon>();
 			this.persoon.add(new Persoon());
 		}
 		this.persoon.get(0).setAnummer(anummer);
 	}
-	
+
 	public void setPersoon(List<Persoon> persoon) {
 		this.persoon = persoon;
 	}
-	
+
 	public List<Persoon> getPersoon() {
 		return this.persoon;
 	}
 
 	public Ouder getCurrentOuder(int ouder) {
-		if( this.ouder == null || this.ouder.size() < ouder || this.ouder.get(ouder).size() == 0 ) {
+		if (this.ouder == null || this.ouder.size() < ouder || this.ouder.get(ouder).size() == 0) {
 			throw new PLException("Need at least a current Ouder" + (ouder + 1));
 		}
 		return this.ouder.get(ouder).get(0);
 	}
-	
+
 	public void setOuder(List<List<Ouder>> ouder) {
 		this.ouder = ouder;
 	}
@@ -59,9 +77,9 @@ public class PersoonsLijst {
 	public List<List<Ouder>> getOuder() {
 		return this.ouder;
 	}
-	
+
 	public List<Ouder> getOuder(int ouder) {
-		if( this.ouder == null || this.ouder.size() < ouder) {
+		if (this.ouder == null || this.ouder.size() < ouder) {
 			return null;
 		}
 		return this.ouder.get(ouder);
@@ -78,9 +96,9 @@ public class PersoonsLijst {
 	public void setVerblijfplaats(List<Verblijfplaats> verblijfplaats) {
 		this.verblijfplaats = verblijfplaats;
 	}
-	
-	public List<Verblijfplaats> getVerblijfplaats(){
+
+	public List<Verblijfplaats> getVerblijfplaats() {
 		return this.verblijfplaats;
 	}
-		
+
 }
