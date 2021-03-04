6b5820155e253ecbfc6663c79f4a8c7d6a9cdb84
Frank Reerink <nospam@reerink.name>
2021-03-04 22:29:26 +0100

    Use SAX parser

--- bag/src/main/java/com/reerinkresearch/bag/LoaderApplication.java
+++ bag/src/main/java/com/reerinkresearch/bag/LoaderApplication.java
@@ -1,18 +1,23 @@
 package com.reerinkresearch.bag;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
 
 @SpringBootApplication
 public class LoaderApplication implements CommandLineRunner {
 
 	@Value("${sleepMillis}")
 	private long sleepMillis;
-	
+
 	private static Logger LOG = LoggerFactory.getLogger(LoaderApplication.class);
 
 	public static void main(String[] args) {
@@ -27,6 +32,18 @@ public class LoaderApplication implements CommandLineRunner {
 		for (int i = 0; i < args.length; ++i) {
 			LOG.info("args[{}]: {}", i, args[i]);
 		}
+
+		// Read an XML file
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser saxParser = factory.newSAXParser();
+		DefaultHandler handler = new DefaultHandler() {
+			public void startElement(String uri, String localName, String qName, Attributes attributes) {
+				LOG.info("Start element: " + qName);
+			}
+		};
+
+		saxParser.parse(args[0], handler);
+
 		LOG.info("Sleeping for " + this.sleepMillis + " millis");
 		Thread.sleep(this.sleepMillis);
 	}
